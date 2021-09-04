16b83d9d4a5f59c525c68e45bf3191fc031c4d86
Rohfel <rohfeladyaraka@gmail.com>
2021-09-04 16:50:34 +0700

    Added broken rotation.

--- entities/box.lua
+++ entities/box.lua
@@ -15,6 +15,8 @@ function Box:new(x, y, width, height, speed, texture)
   self.width = width or 0
   self.height = height or 0
   self.speed = speed
+  self.dt = 0
+  self.rotation = 0 -- in radians
   
   self.quad = love.graphics.newQuad(self.x, self.y, self.width, self.height, 10, 10)
   self.texture = texture
@@ -35,9 +37,18 @@ function Box:update(dt)
   if(love.keyboard.isDown('d')) then
       self.x = self.x + self.speed * dt
   end
+
+  self.dt = dt
+end
+
+function toRadians(degrees)
+  return degrees * 0.01745329251994329576923690768489
 end
 
 -- and the draw() function
 function Box:draw()
+  self.rotation = self.rotation + self.dt * 50
+
+  love.graphics.rotate(toRadians(self.rotation))
   love.graphics.draw(self.texture, self.quad, self.x, self.y)
 end
